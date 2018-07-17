6ae57ac5d6b285791e69fc3115e5e87f7915bceb
ubuntu <hong@coontec.com>
2018-07-16 23:30:07 -0700

    byte pointer

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
-<<<<<<< HEAD
-void winFunc();
-=======
-void linuxFunc();
->>>>>>> 460cdb3103ef3a9573b5779a76f9d95d5f6193d5
+#include <stdio.h>
+
+typedef char* byte_pointer;
+
+void show_byte(byte_pointer start, int len){
+
+	int i;
+	for(i = 0; i < len; i++)
+		printf("0x%x\n", start[i]);
+}
+
+int main(){
+
+	int a = 0x78123456;
+	show_byte((byte_pointer)&a, 1);
+	printf("\n");
+	show_byte((byte_pointer)&a, 2);
+
+	printf("\n");
+	show_byte((byte_pointer)&a, 3);
+
+	printf("\n");
+	show_byte((byte_pointer)&a, 4);
+}
+
